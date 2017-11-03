35051dbac8df35ff35c4424f7c5fdacf271a6fee
lordbird12 <pu.niwat_st@tni.ac.th>
2017-11-03 14:25:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => '��������������������'
+				'text' => 'มีไรทักทำไม มีไรทักทำไมหุบปากเดียวนี้ลำคาญ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
