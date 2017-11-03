4549e1c1fcecc42e9c9c8be9b1bbffbb26928378
lordbird12 <pu.niwat_st@tni.ac.th>
2017-11-03 14:10:21 +0700

    สส

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'what what what'
+				'text' => 'มีไรมีไรมีไรมีไรมีไร'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
