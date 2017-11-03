b1b1efcae3ccd210e78cf8beb95d249c650fda9c
lordbird12 <pu.niwat_st@tni.ac.th>
2017-11-03 15:43:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ if (!is_null($events['events'])) {
 // 			];
 			
 			$messages = [
-				  "type" => "sticker",
-				  "packageId" => "1",
-				  "stickerId" => "1"
+				  'type' => 'sticker',
+				  'packageId' => '1',
+				  'stickerId' => '1'
 				];
 
 			// Make a POST Request to Messaging API to reply to sender
