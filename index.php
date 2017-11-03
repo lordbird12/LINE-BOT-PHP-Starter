3aa56b7370311f4dbd5171044ea50e6e38f45d03
lordbird12 <pu.niwat_st@tni.ac.th>
2017-11-03 15:48:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			
-			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
 
 			// Build message to reply back
 			$messages = [
