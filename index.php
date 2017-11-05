08e58637d0df08d48bbda544b5ee5ce155721363
lordbird12 <pu.niwat_st@tni.ac.th>
2017-11-05 18:10:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if (!is_null($events['events'])) {
 			}else if($text == 'imagemap'){
 				$data = '{
 					  "type": "imagemap",
-					  "baseUrl": "https://dptf.herokuapp.com/300/Deep_Purple_(album).jpeg",
+					  "baseUrl": "https://example.com/bot/images/rm001",
 					  "altText": "this is an imagemap",
 					  "baseSize": {
 					      "height": 1040,
